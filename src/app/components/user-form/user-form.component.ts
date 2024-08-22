9ffabe70d4dc3488e3c007badeb6543814295293
Víctor <Vivivikthor@gmail.com>
2024-08-22 21:16:16 +0200

    Maquetado del formulario

---
+++ src/app/components/user-form/user-form.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './user-form.component.html',
+  styleUrl: './user-form.component.css',
+})
+export class UserFormComponent {
+  userForm: FormGroup;
+  tipo :string ="Nuevo"
+  constructor() {
+    this.userForm = new FormGroup({}, []);
+  }
+}
--- src/app/pages/update-user/update-user.component.ts
+++ src/app/pages/update-user/update-user.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { UserFormComponent } from "../../components/user-form/user-form.component";
 
 @Component({
   selector: 'app-update-user',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, UserFormComponent],
   templateUrl: './update-user.component.html',
   styleUrl: './update-user.component.css'
 })
