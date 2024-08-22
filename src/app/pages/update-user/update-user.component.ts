8c40a9ea574becf4b64da6da9486d5fd341869d4
Víctor <Vivivikthor@gmail.com>
2024-08-22 19:52:17 +0200

    Páginas en marcha

--- src/app/pages/update-user/update-user.component.ts
+++ src/app/pages/update-user/update-user.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-update-user',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './update-user.component.html',
   styleUrl: './update-user.component.css'
 })
