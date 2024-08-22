060b222faf910d9e4d0baebceb967860c72c3afe
Víctor <Vivivikthor@gmail.com>
2024-08-22 17:55:59 +0200

    Router en marcha

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserComponent } from './pages/user/user.component';
+import { UpdateUserComponent } from './pages/update-user/update-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path:'', pathMatch:'full', redirectTo:'home'},
+    {path:'home', component:HomeComponent},
+    {path:'user/:id', component:UserComponent},
+    {path:'update-user/:id', component:UpdateUserComponent},
+    {path:'new-user', component:UpdateUserComponent},
+    {path:'update-user/:id', component:UpdateUserComponent},
+    {path:'**', redirectTo: 'home'}
+];
