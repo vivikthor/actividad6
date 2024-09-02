9b4456e872d83e3206c3993d966587d04cb41474
Víctor <Vivivikthor@gmail.com>
2024-09-02 13:37:51 +0200

    CSS completo

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -3,11 +3,12 @@ import { IUser } from '../../interfaces/iuser.interface';
 import { RouterLink } from '@angular/router';
 import { UserServiceService } from '../../services/user-service.service';
 import { DeleteBtnComponent } from "../delete-btn/delete-btn.component";
+import { UpdateBtnComponent } from "../update-btn/update-btn.component";
 
 @Component({
   selector: 'app-user-card',
   standalone: true,
-  imports: [RouterLink, DeleteBtnComponent],
+  imports: [RouterLink, DeleteBtnComponent, UpdateBtnComponent],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.css',
 })
