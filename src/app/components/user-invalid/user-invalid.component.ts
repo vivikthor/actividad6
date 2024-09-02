dff4e6e07237b26c1c1664e29f13bb2abe491c46
Víctor <Vivivikthor@gmail.com>
2024-09-02 14:31:03 +0200

    Mejoras de robustez

---
+++ src/app/components/user-invalid/user-invalid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-invalid',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-invalid.component.html',
+  styleUrl: './user-invalid.component.css'
+})
+export class UserInvalidComponent {
+
+}
--- src/app/pages/update-user/update-user.component.ts
+++ src/app/pages/update-user/update-user.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
-import { NavbarComponent } from "../../components/navbar/navbar.component";
-import { UserFormComponent } from "../../components/user-form/user-form.component";
+import { Component, inject } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { UserFormComponent } from '../../components/user-form/user-form.component';
+import { ActivatedRoute } from '@angular/router';
+import { IUser } from '../../interfaces/iuser.interface';
+import { UserServiceService } from '../../services/user-service.service';
+import { UserInvalidComponent } from '../../components/user-invalid/user-invalid.component';
 
 @Component({
   selector: 'app-update-user',
   standalone: true,
-  imports: [NavbarComponent, UserFormComponent],
+  imports: [NavbarComponent, UserFormComponent, UserInvalidComponent],
   templateUrl: './update-user.component.html',
-  styleUrl: './update-user.component.css'
+  styleUrl: './update-user.component.css',
 })
 export class UpdateUserComponent {
+  userService = inject(UserServiceService);
+  activatedRoute = inject(ActivatedRoute);
+  activatedUser!: IUser;
 
+  // checkUser(user : IUser): boolean {
+  //   if(user === null){
+  //     return false
+  //   }else{
+  //     return true
+  //   }
+  // }
+
+  ngOnInit() {
+    console.log(this.activatedUser);
+    this.activatedRoute.params.subscribe(async (params) => {
+      this.activatedUser = await this.userService.getById(params['id']);
+    });
+  }
 }
